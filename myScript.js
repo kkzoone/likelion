72885eb78927fc87c6137325cf5cbc04dfd62b1a
kkzoone <kyungjun1129@naver.com>
2022-04-02 23:16:24 +0900

    second commit

---
+++ myScript.js
@@ -0,0 +1,3 @@
+// document.write('자바스크립트 짱')
+// 1. html 파일 body가 끝나는 지점에 <script></script>태그를 작성하고 그 안에 documnet.write()를 작성하고 사용하면 된다
+// html 파일 body에 <script></script>태그를 작성하고 자바스크립트 경로를 연결해주고 사용하면된다.
